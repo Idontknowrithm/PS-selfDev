20f66c5710110f17e9e52ee70264fe0f0e7a1d05
Yuhyun Kim <53094801+Idontknowrithm@users.noreply.github.com>
2021-03-20 14:58:01 +0900

    Update BOJ-19947-투자의_귀재_배주형.cpp

--- BOJ/BOJ-19947-투자의_귀재_배주형.cpp
+++ BOJ/BOJ-19947-투자의_귀재_배주형.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<cmath>
 
 double H, Y, year[30];
 
@@ -6,14 +7,16 @@ double H, Y, year[30];
 double DP(){
     // 1Y -> 5% 3Y -> 20% 5Y -> 35%
     for(int i = 0; i < Y; ++i){
-        year[i + 1] = (year[i + 1] < (H + year[i]) * 0.05) ? (H + year[i]) * 0.05 : year[i + 1];
-        year[i + 3] = (year[i + 3] < (H + year[i]) * 0.20) ? (H + year[i]) * 0.20 : year[i + 1];
-        year[i + 5] = (year[i + 5] < (H + year[i]) * 0.35) ? (H + year[i]) * 0.35 : year[i + 1];
+        year[i + 1] = floor((year[i + 1] < year[i] * 1.05) ? year[i] * 1.05 : year[i + 1]);
+        year[i + 3] = floor((year[i + 3] < year[i] * 1.20) ? year[i] * 1.20 : year[i + 3]);
+        year[i + 5] = floor((year[i + 5] < year[i] * 1.35) ? year[i] * 1.35 : year[i + 5]);
     }
+    return year[(int)Y];
 }
 
 int main() {
     scanf("%lf %lf", &H, &Y);
-    printf("%.0lf", DP() + H);
+    year[0] = H;
+    printf("%.0lf", floor(DP()));
     return 0;
 }
