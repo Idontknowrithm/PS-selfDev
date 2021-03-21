f7a51ae4bb386b29fef9a1178b3cf87390ba58f6
Yuhyun Kim <53094801+Idontknowrithm@users.noreply.github.com>
2021-03-21 14:52:12 +0900

    Update AOJ-QUANTIZE.cpp

--- AOJ/AOJ-QUANTIZE.cpp
+++ AOJ/AOJ-QUANTIZE.cpp
@@ -1,6 +1,5 @@
 #include<iostream>
 #include<algorithm>
-#include<cmath>
 
 const int INF = 999999999;
 int T, N, S, seq[105], cache[105][15];
@@ -10,29 +9,26 @@ int sqrt_err(int lo, int hi){ // [lo, hi)
     
     for(int i = lo; i < hi; ++i)
         sum += seq[i];
-    mid = (int)round((double)sum / (hi - lo));
+    mid = (int)((double)sum / (hi - lo) + 0.5);
     for(int i = lo; i < hi; ++i){
         err += (seq[i] - mid) * (seq[i] - mid);
     }
     return err;
 }
 int DP(int start, int part){
-    if(start == N) return 0;
-    if(part == 0) return 0;
+    if(start == N - 1) return 0;
 
     int& ret = cache[start][part];
     if(ret != -1) return ret;
     ret = INF;
 
-    for(int i = 1; i + start <= N; ++i)
-        ret = std::min(ret, sqrt_err(start, start + i) + DP(start + i, part - 1));
+    if(part != 1)
+        for(int i = 1; i + start < N; ++i){
+            ret = std::min(ret, sqrt_err(start, start + i) + DP(start + i, part - 1));
+        }
+    else 
+        ret = sqrt_err(start, N);
     
-    printf("arr\n");
-    for(int i = 0; i < 4; ++i){
-        for(int u = 0; u < 10; ++u)
-            printf("%d ",cache[u][i]);
-        puts("");
-    }
     return ret;
 }
 
@@ -45,7 +41,6 @@ int main() {
         for(int i = 0; i < 105; ++i)
         for(int u = 0; u < 15; ++u)
             cache[i][u] = -1;
-        printf("///////////////////////////////\n");
         printf("%d\n", DP(0, S));
     }
     return 0;
