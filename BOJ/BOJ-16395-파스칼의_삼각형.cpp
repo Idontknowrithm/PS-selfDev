5ed3de32ac09ed06dfb703e00d808a4d64bad507
Yuhyun Kim <53094801+Idontknowrithm@users.noreply.github.com>
2021-03-23 19:37:16 +0900

    Update BOJ-16395-파스칼의_삼각형.cpp

--- BOJ/BOJ-16395-파스칼의_삼각형.cpp
+++ BOJ/BOJ-16395-파스칼의_삼각형.cpp
@@ -1,11 +1,16 @@
 #include<iostream>
 
-long long N, K, ans = 1;
+long long N, K, arr[35][35];
 
 int main() {
     scanf("%lld %lld", &N, &K);
-    for(int i = N - 1; i > N - K; --i) ans *= i;
-    for(int i = 1; i < K; ++1)     ans /= i;
-    printf("%lld", ans);
+    for(int i = 0; i <= 30; ++i) arr[0][i] = 1;
+    for(int i = 1; i < N; ++i){
+        arr[i][0] = 1;
+        for(int u = 1; u < K; ++u){
+            arr[i][u] = arr[i - 1][u] + arr[i][u - 1];
+        }
+    }
+    printf("%lld", arr[N - K][K - 1]);
     return 0;
 }
