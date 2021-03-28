a4816f0670d8345a65692bc4130e43856895b7c3
Yuhyun Kim <53094801+Idontknowrithm@users.noreply.github.com>
2021-03-28 14:14:15 +0900

    Update BOJ-1013-Contact.cpp

--- BOJ/BOJ-1013-Contact.cpp
+++ BOJ/BOJ-1013-Contact.cpp
@@ -7,29 +7,29 @@ std::string input;
 int parser_100_1_(int start){
     int i, zero = 0, one = 0;
     for(i = start + 1; i < input.size() && input[i] == '0'; ++i) ++zero;
-    if(i == input.size()) return -1; // impossible
+    if(i == input.size() || zero < 2) return -1; // impossible
     for(; i < input.size() && input[i] == '1'; ++i) ++one;
     if(i == input.size()) return -2; // possible
-    return i;
+    return i - 1;
 }
 
 void exsearch(int start){
     if(start == input.size()){possible = true; return;}
     int parser_ret;
-    for(int i = start; i < input.size(); ++i){
-        if(input[start] == '0'){
-            if(start + 1 < input.size() && input[start + 1] == '1')
-                exsearch(start + 2);
-            else if(start + 1 < input.size())
-                return;
-        }
-        else if(input[start] == '1'){
-            parser_ret = parser_100_1_(start);
-            if(parser_ret == -1) return;
-            else if(parser_ret == -2){possible = true; return;}
-            i = parser_ret;
-            exsearch(i);
-        }
+    if(input[start] == '0'){
+        if(start + 1 < input.size() && input[start + 1] == '1')
+            exsearch(start + 2);
+        else if(start + 1 < input.size())
+            return;
+    }
+    else if(input[start] == '1'){
+        parser_ret = parser_100_1_(start);
+        if(parser_ret == -1) return;
+        else if(parser_ret == -2){possible = true; return;}
+
+        if(input[parser_ret - 1] == '1')
+            exsearch(parser_ret);
+        exsearch(parser_ret + 1);
     }
 }
 
