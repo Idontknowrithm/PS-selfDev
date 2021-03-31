9d651495e903b614420f55dc1fe1568444726a33
Yuhyun Kim <53094801+Idontknowrithm@users.noreply.github.com>
2021-03-31 20:16:10 +0900

    Update PRG-skill_check-level2-2.cpp

--- PRG/PRG-skill_check-level2-2.cpp
+++ PRG/PRG-skill_check-level2-2.cpp
@@ -1,81 +1,50 @@
 #include <string>
 #include <vector>
-#include<iostream>
 
-typedef struct{
-    int lan, where, career, food, points;
-}volunteer;
-// lan-> 1: cpp, 2: java, 3: python
-// where-> 1: back, 2: front
-// career-> 1: junior, 2: senior
-// food-> 1: chicken, 2: pizza
-int lan(char t){
-    if(t == 'c')      return 1;
-    else if(t == 'j') return 2;
-    else if(t == 'p') return 3;
-    else              return 0;
-}
-int where(char t){
-    if(t == 'b') return 1;
-    else if(t == 'f') return 2;
-    else              return 0;
-}
-int career(char t){
-    if(t == 'j') return 1;
-    else if(t == 's') return 2;
-    else              return 0;
-}
-int food(char t){
-    if(t == 'c') return 1;
-    else if(t == 'p') return 2;
-    else              return 0;
-}
-bool possible(volunteer p, volunteer line){
-    int can = 0;
-    if(line.lan == 0 || line.lan == p.lan) can++;
-    if(line.where == 0 || line.where == p.where) can++;
-    if(line.career == 0 || line.career == p.career) can++;
-    if(line.food == 0 || line.food == p.food) can++;
-    if(line.points <= p.points) can++;
-    return (can == 5) ? true : false;
-}
-std::vector<int> solution(std::vector<std::string> info, std::vector<std::string> query) {
-    std::vector<volunteer> people;
-    std::vector<int> answer;
-    std::cout << "k";
-    for(int i = 0; i < info.size(); ++i){
-        volunteer tmp;
-        int space;
-        tmp.lan = lan(info[i][0]);
-        space = info[i].find(" ", 0) + 1;
-        tmp.where = where(info[i][space]);
-        space = info[i].find(" ", space) + 1;
-        tmp.career = career(info[i][space]);
-        space = info[i].find(" ", space) + 1;
-        tmp.food = food(info[i][space]);
-        space = info[i].find(" ", space) + 1;
-        tmp.points = stoi(info[i].substr(space));
-        people.push_back(tmp);
+using namespace std;
+
+int intlen(int a){
+    int tmp = 0; ++a;
+    if(a == 1) return 0;
+    while(a != 0){
+        a /= 10;
+        ++tmp;
     }
-    for(int i = 0; i < query.size(); ++i){
-        volunteer tmp;
-        int space;
-        tmp.lan = lan(info[i][0]);
-        space = info[i].find(" and ", 0) + 5;
-        tmp.where = where(info[i][space]);
-        space = info[i].find(" and ", 0) + 5;
-        tmp.career = career(info[i][space]);
-        space = info[i].find(" and ", 0) + 5;
-        tmp.food = food(info[i][space]);
-        space = info[i].find(" ", space) + 1;
-        tmp.points = stoi(info[i].substr(space));
-        int can = 0;
-        for(int u = 0; u < people.size(); ++u){
-            if(possible(people[u], tmp)) ++can;
+    return tmp;
+}
+
+int solution(string s) {
+    int answer = 1000000;
+    if(s.size() == 1) return 1;
+    for(int i = 1; i <= s.size() / 2; ++i){
+        string tmp;
+        int ptr = 0, again = 0, zipped_size = 0;
+        tmp = s.substr(ptr, i);
+        while(ptr < s.size()){
+            ptr += i;
+            if(ptr + i >= s.size()){
+                if(tmp == s.substr(ptr)){
+                    again++;
+                    zipped_size += intlen(again) + tmp.size();
+                }
+                else{
+                    zipped_size += intlen(again) + tmp.size();
+                    zipped_size += s.substr(ptr).size();
+                }
+                break;
+            }
+            else{
+                if(tmp == s.substr(ptr, i)){
+                    again++;
+                }
+                else{
+                    zipped_size += intlen(again) + tmp.size();
+                    again = 0;
+                    tmp = s.substr(ptr, i);
+                }    
+            }
         }
-        answer.push_back(can);
+        answer = (answer > zipped_size) ? zipped_size : answer;
     }
-    
-    
     return answer;
 }
