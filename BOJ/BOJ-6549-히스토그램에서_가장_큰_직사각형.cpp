8a0d3b6fef43aed9154d29db81bdd78d7cb801b6
Yuhyun Kim <53094801+Idontknowrithm@users.noreply.github.com>
2021-05-22 23:45:14 +0900

    Create BOJ-6549-히스토그램에서_가장_큰_직사각형.cpp

---
+++ BOJ/BOJ-6549-히스토그램에서_가장_큰_직사각형.cpp
@@ -0,0 +1,93 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<limits>
+#define ll long long
+#define pp std::pair<unit, unit>
+
+typedef ll unit;
+
+const unit unit_max = std::numeric_limits<unit>::max();
+
+unit N;
+std::vector<pp> arr;
+
+class segment_tree{
+    unit N;
+    std::vector<pp> seq;
+    public:
+        segment_tree(const std::vector<pp> &arr){
+            N = arr.size() - 1;
+            seq.resize(N * 4);
+            base();
+            init(arr, 1, N, 1);
+        }
+        void base(){
+            for(int i = 0; i < N * 4; ++i)
+                seq[i] = {i, unit_max};
+        }
+        pp init(const std::vector<pp> &arr, unit left, unit right, unit root){
+            if(left == right){
+              seq[root] = arr[left];
+              return seq[root];
+            }
+            unit mid = (left + right) / 2;
+            pp left_min = init(arr, left, mid, root * 2);
+            pp right_min = init(arr, mid + 1, right, root * 2 + 1);
+            if(left_min.second < right_min.second)
+                seq[root] = left_min;
+            else 
+                seq[root] = right_min;
+            return seq[root];
+        }
+        pp query(unit left, unit right, unit root, unit this_left, unit this_right){
+            if(right < this_left || this_right < left)
+                return {-1, 0};
+            if(left <= this_left && this_right <= right)
+                return seq[root];
+            
+            unit mid = (this_left + this_right) / 2;
+            pp leftq = query(left, right, root * 2, this_left, mid);
+            pp rightq = query(left, right, root * 2 + 1, mid + 1, this_right);
+            if(leftq.second < rightq.second)
+                return leftq;
+            else 
+                return rightq;
+        }
+        pp query(unit left, unit right){
+            return query(left, right, 1, 1, N);
+        }
+};
+
+unit DQ(segment_tree &st, pp divider, unit left, unit right){
+    puts("ok");
+    if(left > right) return 0;
+    if(left == right) return divider.second;
+    unit max = divider.second * (right - left + 1);
+    pp left_div = st.query(left, divider.first - 1);
+    max = std::max(max, DQ(st, left_div, left, divider.first - 1));
+    pp right_div = st.query(divider.first + 1, right);
+    max = std::max(max, DQ(st, right_div, divider.first + 1, right));
+    return max;
+}
+
+int main() {
+    while(1){
+        unit input, ans = 0;
+        scanf("%lld", &N);
+        if(!N) return 0;
+        arr.push_back({0, unit_max});
+        for(unit i = 1; i <= N; ++i){
+            scanf("%lld", &input);
+            arr.push_back({i, input});
+        }
+        segment_tree segtree(arr);
+        pp tmp = segtree.query(1, N);
+
+        ans = DQ(segtree, tmp, 1, N);
+        
+        printf("%lld\n", ans);
+        arr.clear();
+    }
+    return 0;
+}
