f8478dc26d0a7301551aaf37db2591df4849c234
Yuhyun Kim <53094801+Idontknowrithm@users.noreply.github.com>
2021-05-27 20:33:30 +0900

    Create BOJ-1208-부분수열의_합_2.cpp

---
+++ BOJ/BOJ-1208-부분수열의_합_2.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+int N, S, ans, seq1_size, seq2_size, seq1[45], seq2[45];
+std::vector<int> sum1, sum2;
+
+bool cmp(const int &a, const int &b){
+    return a > b;
+}
+void make_sum1(int idx, int prev_sum){
+    if(idx == seq1_size){
+        sum1.push_back(prev_sum);
+        return;
+    }
+    make_sum1(idx + 1, prev_sum);
+    make_sum1(idx + 1, prev_sum + seq1[idx]);
+}
+void make_sum2(int idx, int prev_sum){
+    if(idx == seq2_size){
+        sum2.push_back(prev_sum);
+        return;
+    }
+    make_sum2(idx + 1, prev_sum);
+    make_sum2(idx + 1, prev_sum + seq2[idx]);
+}
+
+int main() {
+    scanf("%d %d", &N, &S);
+    for(int i = 0; i < N; ++i)
+        if(i <= N / 2)
+            scanf("%d", &seq1[i]);
+        else 
+            scanf("%d", &seq2[i - N / 2 - 1]);
+    seq1_size = N / 2;
+    seq2_size = N / 2;
+    (N % 2 == 1) ? (++seq1_size) : 0;
+    make_sum1(0, 0);
+    make_sum2(0, 0);
+    std::sort(sum1.begin(), sum1.end());
+    std::sort(sum2.begin(), sum2.end(), cmp);
+
+    int ptr1 = 0, ptr2 = 0;
+    while(ptr1 != sum1.size() && ptr2 != sum2.size()){
+        if(sum1[ptr1] + sum2[ptr2] == S) ++ans;
+        if(ptr1 != sum1.size() - 1 && sum1[ptr1] == sum1[ptr1 + 1]) ++ptr1;
+        else if(ptr2 != sum2.size() - 1 && sum2[ptr2] == sum2[ptr2 + 1]) ++ptr2;
+        else if(sum1[ptr1] + sum2[ptr2] < S) ++ptr1;
+        else                            ++ptr2;
+    }
+    if(S == 0) --ans;
+    printf("%d", ans);
+    return 0;
+}
