977cf4d2fe5d97b3e6d79f0328dc2dfc9f874d5c
Yuhyun Kim <53094801+Idontknowrithm@users.noreply.github.com>
2021-06-09 19:57:14 +0900

    Update BOJ-17404-RGB거리_2.cpp

--- BOJ/BOJ-17404-RGB거리_2.cpp
+++ BOJ/BOJ-17404-RGB거리_2.cpp
@@ -1,3 +1,7 @@
+// 시작 색을 결정하기만 하면 되는데 시작 색에 따라 끝 색을 결정해버림
+// -> 시작 색이 R일 때 끝 색의 G, B 중 더 작은 값을 고정하고 DP함
+// 과도한 제한인 지 확인해보고 제한 조건 걸기
+
 #include<iostream>
 #include<algorithm>
 #define FRT first_RGB_tracker
@@ -13,33 +17,14 @@ int SFS(int start){
         DP[i][1] = RGB[i][1];
         DP[i][2] = RGB[i][2];
     }
-    for(int i = 0; i < 3; ++i)
+    for(int i = 0; i < 3; ++i){
         if(i != start) DP[0][i] = INF;
-
-    switch(start){
-        case 0:
-            if(DP[N - 1][1] < DP[N - 1][2])
-                DP[N - 2][1] = DP[N - 1][0] = DP[N - 1][2] = INF;
-            else 
-                DP[N - 2][2] = DP[N - 1][0] = DP[N - 1][1] =  INF;
-            break;
-        case 1:
-            if(DP[N - 1][0] < DP[N - 1][2])
-                DP[N - 2][0] =  DP[N - 1][1] = DP[N - 1][2] = INF;
-            else 
-                DP[N - 2][2] =  DP[N - 1][0] = DP[N - 1][1] = INF;
-            break;
-        case 2:
-            if(DP[N - 1][0] < DP[N - 1][1])
-                DP[N - 2][0] =  DP[N - 1][1] = DP[N - 1][2] = INF;
-            else 
-                DP[N - 2][1] =  DP[N - 1][0] = DP[N - 1][1] = INF;
-            break;
+        if(i == start) DP[N - 1][i] = INF;
     }
     for(int i = 1; i < N; ++i){
         DP[i][0] += std::min(DP[i - 1][1], DP[i - 1][2]);
         DP[i][1] += std::min(DP[i - 1][0], DP[i - 1][2]);
-        DP[i][2] += std::min(DP[i - 1][1], DP[i - 1][0]);
+        DP[i][2] += std::min(DP[i - 1][0], DP[i - 1][1]);
     }
     return std::min(std::min(DP[N - 1][0], DP[N - 1][1]), DP[N - 1][2]);
 }
