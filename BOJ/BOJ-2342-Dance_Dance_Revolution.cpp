61b7bb04b9cd7ed53043127c12a3a87ec479722a
Yuhyun Kim <53094801+Idontknowrithm@users.noreply.github.com>
2021-06-15 20:30:08 +0900

    Update BOJ-2342-Dance_Dance_Revolution.cpp

--- BOJ/BOJ-2342-Dance_Dance_Revolution.cpp
+++ BOJ/BOJ-2342-Dance_Dance_Revolution.cpp
@@ -1,38 +1,44 @@
+// DP는 캐시 테이블, 점화식 두 개가 전부
+// 캐시 테이블이 3차원, 4차원일 수도 있으니 굳이 1차원, 2차원으로 
+// 줄이기가 힘들다면 3, 4차원도 생각을 해볼 수 있도록 
+
 #include<iostream>
 #include<vector>
 #include<algorithm>
-#define pp std::pair<int, int>
-#define itpr interpreter
+#include<cmath>
 
 const int INF = 400005;
-int N, cache[100005][40];
-std::vector<int> instruct;
+int N, cache[5][5][100005];
+std::vector<int> input;
 
-int itpr(pp i){
-    return i.first * 10 + i.second;
+int interpreter(int cur, int next){
+    if(!cur)                      return 2;
+    else if(cur == next)          return 1;
+    else if(abs(cur - next) == 2) return 4;
+    else                          return 3;
 }
+int DP(int l, int r, int cur){
+    if(cur == N) return 0;
 
-int DP(pp status, int inst){
-    if(inst == N) return 0;
-    pp tmp;
-    int &ret = cache[itpr(status)][inst];
+    int &ret = cache[l][r][cur];
     if(ret) return ret;
 
     ret = INF;
-    if(status.first < instruct[inst])
-        tmp = {status.first, instruct[inst]};
-    else 
-        tmp = {instruct[inst], status.first};
+
+    ret = std::min(ret, DP(input[cur], r, cur + 1) + interpreter(l, input[cur]));
+    ret = std::min(ret, DP(l, input[cur], cur + 1) + interpreter(r, input[cur]));
+
+    return ret;
 }
 
 int main() {
-    int input = -1;
-    scanf("%d", &input);
-    while(input){
-        instruct.push_back(input);
-        scanf("%d", &input);
+    int tmp = -1;
+    scanf("%d", &tmp);
+    while(tmp){
+        input.push_back(tmp);
+        scanf("%d", &tmp);
     }
-    N = instruct.size();
-    printf("%d", DP({0, 0}, 0));
+    N = input.size();
+    printf("%d", DP(0, 0, 0));
     return 0;
 }
