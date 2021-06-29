720da6fe5eee567d2a8f53102571579f24755790
Yuhyun Kim <53094801+Idontknowrithm@users.noreply.github.com>
2021-06-29 19:57:22 +0900

    Update BOJ-17143-낚시왕.cpp

--- BOJ/BOJ-17143-낚시왕.cpp
+++ BOJ/BOJ-17143-낚시왕.cpp
@@ -1,27 +1,25 @@
+// 상어를 하나씩 움직이다가 반례에 걸림
+// 상어를 하나씩 움직이면서 그때 그때 만난 상어들에 대해서 그 즉시 처리해줬는데
+// 하나씩 처리해주면 안되고 모든 상어가 움직이고 나서 한 번에 잡아먹는 처리를 해줬어야 함
+// 문제를 읽고 하니씩 그때 그때 처리해도 되는지, 한꺼번에 처리해줘야 하는지 
+
 #include<iostream>
 #include<vector>
 #include<algorithm>
+#include<cstring>
 
 typedef struct{
     int r, c, s, d, z; // row, col, speed, direction, size
 }shark;
 
 shark obj[10005];
-int R, C, M, ans, fishing[105][105];
+int R, C, M, ans, fishing[105][105], tmp[105][105];
 
 // 1 2 3 4
 // ^ v > <
 
 void sharks_are_moving(){
     for(int i = 1; i <= M; ++i){
-        /*
-         * (현재 장소 + 속도 - 1) / (낚시터 격자 크기 - 1)
-         *  홀수면 방향 그대로, 짝수면 방향 변경
-         * 
-         * (현재 장소 + 속도 - 1) % (낚시터 격자 크기 - 1)
-         * 최종 방향 반대 방향의 맨 끝에서 나머지 만큼 더해주면서
-         * 값이 0이 아니면 방향 변경
-         */
         if(!obj[i].z)
             continue;
         fishing[obj[i].r][obj[i].c] = 0;
@@ -32,10 +30,10 @@ void sharks_are_moving(){
         else if(obj[i].d == 2 && obj[i].r == R) obj[i].d = 1;
         switch(obj[i].d){
             case 1: // ^
-                div = ((R - 1) + (R - obj[i].r + 1) + obj[i].s) / (R - 1);
-                mod = ((R - 1) + (R - obj[i].r + 1) + obj[i].s) % (R - 1);
                 obj[i].d = 2;
-
+                div = (R * 2 - obj[i].r + obj[i].s - 1) / (R - 1);
+                mod = (R * 2 - obj[i].r + obj[i].s - 1) % (R - 1);
+                
                 if(div & 1){
                     obj[i].d = 1;
                     obj[i].r = R;
@@ -43,17 +41,11 @@ void sharks_are_moving(){
                 else{
                     obj[i].r = 1;
                 }
-                if(mod){
-                    obj[i].r += (obj[i].d == 2) ? mod - 1 : -(mod - 1);
-                }
-                else{
-                    obj[i].r += (obj[i].d == 2) ? 1 : -1;
-                    obj[i].d = (obj[i].d == 2) ? 1 : 2;
-                }
+                obj[i].r += (obj[i].d == 2) ? mod : -mod;
                 break;
             case 2: // v
-                div = (obj[i].r + obj[i].s) / (R - 1);
-                mod = (obj[i].r + obj[i].s) % (R - 1);
+                div = (obj[i].r + obj[i].s - 1) / (R - 1);
+                mod = (obj[i].r + obj[i].s - 1) % (R - 1);
                 
                 if(div & 1){
                     obj[i].d = 1;
@@ -62,17 +54,11 @@ void sharks_are_moving(){
                 else{
                     obj[i].r = 1;
                 }
-                if(mod){
-                    obj[i].r += (obj[i].d == 2) ? mod - 1 : -(mod - 1);
-                }
-                else{
-                    obj[i].r += (obj[i].d == 2) ? 1 : -1;
-                    obj[i].d = (obj[i].d == 2) ? 1 : 2;
-                }
+                obj[i].r += (obj[i].d == 2) ? mod : -mod;
                 break;
             case 3: // >
-                div = (obj[i].c + obj[i].s) / (C - 1);
-                mod = (obj[i].c + obj[i].s) % (C - 1);
+                div = (obj[i].c + obj[i].s - 1) / (C - 1);
+                mod = (obj[i].c + obj[i].s - 1) % (C - 1);
                 
                 if(div & 1){
                     obj[i].d = 4;
@@ -81,19 +67,13 @@ void sharks_are_moving(){
                 else{
                     obj[i].c = 1;
                 }
-                if(mod){
-                    obj[i].c += (obj[i].d == 3) ? mod - 1 : -(mod - 1);
-                }
-                else{
-                    obj[i].c += (obj[i].d == 3) ? 1 : -1;
-                    obj[i].d = (obj[i].d == 3) ? 4 : 3;
-                }
+                obj[i].c += (obj[i].d == 3) ? mod : -mod;
                 break;
             case 4: // <
-                div = ((C - 1) + (C - obj[i].c + 1) + obj[i].s) / (C - 1);
-                mod = ((C - 1) + (C - obj[i].c + 1) + obj[i].s) % (C - 1);
                 obj[i].d = 3;
-
+                div = (C * 2 - obj[i].c + obj[i].s - 1) / (C - 1);
+                mod = (C * 2 - obj[i].c + obj[i].s - 1) % (C - 1);
+                
                 if(div & 1){
                     obj[i].d = 4;
                     obj[i].c = C;
@@ -101,28 +81,29 @@ void sharks_are_moving(){
                 else{
                     obj[i].c = 1;
                 }
-                if(mod){
-                    obj[i].c += (obj[i].d == 3) ? mod - 1 : -(mod - 1);
-                }
-                else{
-                    obj[i].c += (obj[i].d == 3) ? 1 : -1;
-                    obj[i].d = (obj[i].d == 3) ? 4 : 3;
-                }
+                obj[i].c += (obj[i].d == 3) ? mod : -mod;
                 break;
         }
-        if(fishing[obj[i].r][obj[i].c]){
-            if(obj[fishing[obj[i].r][obj[i].c]].z < obj[i].z){
-                obj[fishing[obj[i].r][obj[i].c]].z = 0;
-                fishing[obj[i].r][obj[i].c] = i;
+    }
+    memset(tmp, 0, sizeof(tmp));
+    for(int i = 1; i <= M; ++i){
+        if(!obj[i].z) continue;
+        if(tmp[obj[i].r][obj[i].c]){
+            if(obj[tmp[obj[i].r][obj[i].c]].z < obj[i].z){
+                obj[tmp[obj[i].r][obj[i].c]].z = 0;
+                tmp[obj[i].r][obj[i].c] = i;
             }
             else{
                 obj[i].z = 0;
             }
         }
         else{
-            fishing[obj[i].r][obj[i].c] = i;
+            tmp[obj[i].r][obj[i].c] = i;
         }
     }
+    for(int i = 1; i <= R; ++i)
+    for(int u = 1; u <= C; ++u)
+        fishing[i][u] = tmp[i][u];
 }
 
 int main() {
