c4ad18bd650caf067d8e6f55b16c5849a22868b2
Yuhyun Kim <53094801+Idontknowrithm@users.noreply.github.com>
2021-07-01 20:16:47 +0900

    Create BOJ-1799-비숍.cpp

---
+++ BOJ/BOJ-1799-비숍.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+int N, board[15][15];
+
+int main() {
+    scanf("%d", &N);
+    for(int i = 0; i < N; ++i)
+    for(int u = 0; u < N; ++u)
+        scanf("%d", &board[i][u]);
+    
+    
+}
