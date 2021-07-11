0844457a47e34607e41a863fd04299eb4bb8d13e
Yuhyun Kim <53094801+Idontknowrithm@users.noreply.github.com>
2021-07-11 10:05:16 +0900

    Update BOJ-2533-사회망_서비스(SNS).cpp

--- BOJ/BOJ-2533-사회망_서비스(SNS).cpp
+++ BOJ/BOJ-2533-사회망_서비스(SNS).cpp
@@ -1,3 +1,8 @@
+// 그래프를 트리화 하는 문제였는데 그래프 자체로 visited 배열 써서 푸니까 틀림
+// 그래프를 단방향 그래프화(트리화) 하니까 풀림..
+// 트리 형태로 풀어야 하는 문제에서 DFS 사용할 때는 스택이나 재귀 사용 시 중간에서 꼬일 수 있으니
+// 단방향 그래프로 변형한 뒤 풀어야 할 듯
+
 #include<iostream>
 #include<vector>
 #include<algorithm>
@@ -6,37 +11,36 @@
 
 const int MAX = 1000005, INF = 1000000000;
 int N, ans, visited[MAX], cache[MAX][5];
-std::vector<int> graph[MAX];
+std::vector<int> graph[MAX], tree[MAX];
 
+void one_way_graphing(int node){
+    ++visited[node];
+    for(int i = 0; i < graph[node].size(); ++i){
+        int next = graph[node][i];
+        if(!visited[next]){
+            tree[node].push_back(next);
+            one_way_graphing(next);
+        }
+    }
+}
 int num_of_early(int cur_node, int early_bool){
-    int all_visited = 0;
-    for(int i = 0; i < graph[cur_node].size(); ++i)
-        if(visited[graph[cur_node][i]]) ++all_visited;
-    if(all_visited == graph[cur_node].size()) 
+    if(tree[cur_node].size() == 0)
         return early_bool;
     
-
     int &ret = cache[cur_node][early_bool];
-    if(ret) return ret;
+    if(ret != -1) return ret;
     ret = INF;
 
-    ++visited[cur_node];
     int tmp = early_bool;
     if(early_bool){
-        for(int i = 0; i < graph[cur_node].size(); ++i){
-            if(!visited[graph[cur_node][i]]){
-                ++visited[graph[cur_node][i]];
-                tmp += std::min(num_of_early(graph[cur_node][i], 1),
-                                num_of_early(graph[cur_node][i], 0));
-            }
+        for(int i = 0; i < tree[cur_node].size(); ++i){
+            tmp += std::min(num_of_early(tree[cur_node][i], 1),
+                            num_of_early(tree[cur_node][i], 0));
         }
     }
     else{
-        for(int i = 0; i < graph[cur_node].size(); ++i){
-            if(!visited[graph[cur_node][i]]){
-                ++visited[graph[cur_node][i]];
-                tmp += num_of_early(graph[cur_node][i], 1);
-            }
+        for(int i = 0; i < tree[cur_node].size(); ++i){
+            tmp += num_of_early(tree[cur_node][i], 1);
         }
     }
     ret = std::min(ret, tmp);
@@ -51,9 +55,14 @@ int main() {
         graph[start].push_back(end);
         graph[end].push_back(start);
     }
+    one_way_graphing(1);
+    memset(visited, 0, sizeof(visited));
+    memset(cache, -1, sizeof(cache));
+    ++visited[1];
     ans = num_of_early(1, 0);
     memset(visited, 0, sizeof(visited));
-    memset(cache, 0, sizeof(cache));
+    memset(cache, -1, sizeof(cache));
+    ++visited[1];
     ans = std::min(ans, num_of_early(1, 1));
     printf("%d", ans);
     return 0;    
