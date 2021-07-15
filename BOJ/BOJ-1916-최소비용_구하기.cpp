13d4918950f3f73a3d92ce877e4783c9bd2a6e43
Yuhyun Kim <53094801+Idontknowrithm@users.noreply.github.com>
2021-07-15 18:59:50 +0900

    Update BOJ-1916-최소비용_구하기.cpp

--- BOJ/BOJ-1916-최소비용_구하기.cpp
+++ BOJ/BOJ-1916-최소비용_구하기.cpp
@@ -1,3 +1,8 @@
+// 우선순위 큐에 최근에 갱신된 근처 정점들을 마구 넣다보면
+// 이미 최소 비용으로 갱신된 dist 배열을 다시 방문해서 의미없는 탐색을 할 수 있음
+// for문에 들어가서 if함수를 호출만 해도 많은 비용이 들 수 있으므로
+// 이러한 케이스는 for문으로 들어가기 전에 continue 처리를 해줘야 함
+
 #include<iostream>
 #include<algorithm>
 #include<queue>
@@ -28,9 +33,11 @@ int dijkstra(){
         curV = que.top().first;
         curD = que.top().second;
         que.pop();
-
+        
+        if(curD > dist[curV]) continue; // 이 부분이 관건이었음
+        
         for(int i = 0; i < graph[curV].size(); ++i){
-            if(curD <= dist[curV] && dist[graph[curV][i].first] > curD + graph[curV][i].second){
+            if(dist[graph[curV][i].first] > curD + graph[curV][i].second){ // <-- 여기 있었음
                 dist[graph[curV][i].first] = curD + graph[curV][i].second;
                 que.push({graph[curV][i].first, curD + graph[curV][i].second});
             }
