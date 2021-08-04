c9a304112ac3eaf16bebe635f2f5018a74485c5a
Yuhyun Kim <53094801+Idontknowrithm@users.noreply.github.com>
2021-08-04 18:57:35 +0900

    Update CF-HSSC21-22div1+2-B-Reverse_String.cpp

--- COF/CF-HSSC21-22div1+2-B-Reverse_String.cpp
+++ COF/CF-HSSC21-22div1+2-B-Reverse_String.cpp
@@ -1,3 +1,7 @@
+// abcbe
+// abcba
+// 이렇게 두 방향으로 답이 갈릴 수 있는 테케를 생각 못함
+
 #include<iostream>
 
 int N;
@@ -10,15 +14,23 @@ int main() {
         bool ans = false;
         for(int i = 0; i < input.size(); ++i){
             if(input[i] == res[0]){
-                int tmp = i, tmpi = 0;
-                while(i + tmpi < input.size() && tmpi < res.size() && input[i + tmpi] == res[tmpi])
+                int tmpi = 0;
+                while(i + tmpi < input.size() && tmpi < res.size() && input[i + tmpi] == res[tmpi]){
                     ++tmpi;
-                if(tmpi == res.size()){
-                    ans = true;
-                    break;
+                    if(tmpi == res.size()){
+                        ans = true;
+                        break;
+                    }  
+                    int origin = i + tmpi - 2, repl = tmpi;
+                    while(origin >= 0 && repl < res.size() && input[origin] == res[repl]){
+                        --origin; ++repl;
+                    }
+                    if(repl == res.size()){
+                        ans = true;
+                        break;
+                    }
                 }
-                --tmpi;
-                while()
+                if(ans) break;
             }
         }
         if(ans) std::cout << "YES\n";
