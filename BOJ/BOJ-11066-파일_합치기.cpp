d082d30441ec1a190aa802d4dcc6fc0e9dfdcda8
Yuhyun Kim <53094801+Idontknowrithm@users.noreply.github.com>
2021-08-13 23:13:18 +0900

    Update BOJ-11066-파일_합치기.cpp

--- BOJ/BOJ-11066-파일_합치기.cpp
+++ BOJ/BOJ-11066-파일_합치기.cpp
@@ -1,32 +1,36 @@
 #include<iostream>
 #include<cstring>
 #include<algorithm>
+#include<queue>
+#define pp std::pair<int, int>
+#define ll long long
 
-const int INF = 1000000000;
-int T, K, input[505], cache[505][505];
+const ll INF = 5000000000000;
+int T, K;
+ll input[505], sum[505], cache[505][505];
+
+ll DP(int start, int end){
+    if(start == end) return 0;
+    ll &ret = cache[start][end];
+    if(ret) return ret;
+
+    ret = INF;
+    for(int i = start; i < end; ++i){
+        int tmp = (sum[i] - sum[start - 1]) + (sum[end] - sum[i]);
+        ret = std::min(ret, DP(start, i) + DP(i + 1, end) + tmp);
+    }
+    return ret;
+}
 
 int main() {
     for(scanf("%d", &T); T > 0; --T){
         memset(cache, 0, sizeof(cache));
         scanf("%d", &K);
-        for(int i = 0; i < K; ++i)
-            scanf("%d", &input[i]);
-        for(int i = 0; i < K; ++i)
-            cache[i][i] = input[i];
-        for(int i = 1; i < K; ++i){
-            for(int u = 0; u < K - i; ++u){
-                cache[u][u + i] = INF;
-                for(int l = u; l < u + i; ++l){
-                    int tmp = 0;
-                    if(u == l) tmp += cache[u][l];
-                    else       tmp += 2 * cache[u][l];
-                    if(l + 1 == u + i) tmp += cache[l + 1][u + i];
-                    else               tmp += 2 * cache[l + 1][u + i]; 
-                    cache[u][u + i] = std::min(cache[u][u + i], tmp);
-                }
-            }
+        for(int i = 1; i <= K; ++i){
+            scanf("%lld", &input[i]);
+            sum[i] = sum[i - 1] + input[i];
         }
-        printf("%d\n", cache[0][K - 1]);
+        printf("%lld\n", DP(1, K));
     }
     return 0;
 }
