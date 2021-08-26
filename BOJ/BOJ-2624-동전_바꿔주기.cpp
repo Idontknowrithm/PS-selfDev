165f5f6534e910c9648d6bde3394c170473687fd
Yuhyun Kim <53094801+Idontknowrithm@users.noreply.github.com>
2021-08-26 19:32:06 +0900

    Update BOJ-2624-동전_바꿔주기.cpp

--- BOJ/BOJ-2624-동전_바꿔주기.cpp
+++ BOJ/BOJ-2624-동전_바꿔주기.cpp
@@ -1,26 +1,29 @@
+지폐를 동전으로 바꾸는 문제인데, 동전의 수가 제한이 있다는 점이 다르다
+주어진 동전으로 금액 T를 맞추는 DP 함수를 짜야하는데, 재귀로 짜기 위해서
+금액 T에서 동전의 금액 만큼 빼가면서 탐색하는 top down 형태로 함수를 설계하자
+점화식: DP(val) += DP(val - 특정 동전의 값)
+여기서 동전의 종류에 대한 정보를 넣어야하므로
+DP(val, coin) 동전 종류가 coin일 때 val을 만들기 까지 가능한 경우의 수
+
 #include<iostream>
+#include<vector>
 #include<algorithm>
 #include<cstring>
-#include<vector>
 #define pp std::pair<int, int>
 
 int T, K, cache[10005][105];
-std::vector<pp> coins; // val, cnt
+std::vector<pp> coins;
 
-int DP(int val, int coin_idx){
-    if(!val){
-        cache[val][coin_idx] = 1;
-        return 1;
-    }
-    if(coin_idx < 0) return 0;
+int DP(int val, int coin){
+    if(!val) return 1;
+    if(coin < 0) return 0;
 
-    int &ret = cache[val][coin_idx];
+    int &ret = cache[val][coin];
     if(ret != -1) return ret;
-
     ret = 0;
-    for(int i = 0; i <= coins[coin_idx].second; ++i){
-        if(val - coins[coin_idx].first * i >= 0)
-            ret += DP(val - coins[coin_idx].first * i, coin_idx - 1);
+    for(int i = 0; i <= coins[coin].second; ++i){
+        if(val - coins[coin].first * i >= 0)
+            ret += DP(val - coins[coin].first * i, coin - 1);
     }
     return ret;
 }
@@ -32,10 +35,7 @@ int main() {
         scanf("%d %d", &p, &n);
         coins.push_back({p, n});
     }
-    std::sort(coins.begin(), coins.end());
     memset(cache, -1, sizeof(cache));
-    
-    printf("%d\n", DP(T, K - 1));
-    
+    printf("%d", DP(T, K - 1));
     return 0;
 }
