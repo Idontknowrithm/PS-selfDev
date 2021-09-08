af3ebbd849198fe9a073653928b0c93bd18ee7cc
Yuhyun Kim <53094801+Idontknowrithm@users.noreply.github.com>
2021-09-08 18:51:34 +0900

    Update BOJ-2515-전시장.cpp

--- BOJ/BOJ-2515-전시장.cpp
+++ BOJ/BOJ-2515-전시장.cpp
@@ -1,3 +1,7 @@
+// 특별한 생각 없이 선형적인 시간 복잡도로 DP 하려고 했음
+// DP 과정에서 정렬이 필요한 경우, 또는 정렬된 데이터가 주어지는 경우
+// 이분 탐색을 이용한 그리디를 생각해볼 필요가 있다
+
 #include<iostream>
 #include<cstring>
 #include<vector>
@@ -5,31 +9,18 @@
 #define pp std::pair<int, int>
 
 const int MAX = 300005;
-int N, S, cache[MAX][2];
+int N, S, cache[MAX];
 std::vector<pp> images;
+std::vector<int> height;
 
-int DP(int idx, int incl, int prev){
+int DP(int idx){
     if(idx == N) return 0;
 
-    int &ret = cache[idx][incl];
+    int &ret = cache[idx];
     if(ret != -1) return ret;
-
-    ret = 0;
-    if(incl){
-        if(images[idx].first - prev >= S){
-            ret = std::max(ret, DP(idx + 1, 0, images[idx].first) + images[idx].second);
-            ret = std::max(ret, DP(idx + 1, 1, images[idx].first) + images[idx].second);
-        }
-        else{
-            ret = std::max(ret, DP(idx + 1, 0, images[idx].first));
-            ret = std::max(ret, DP(idx + 1, 1, images[idx].first));
-        }
-    }
-    else{
-        ret = std::max(ret, DP(idx + 1, 0, prev));
-        ret = std::max(ret, DP(idx + 1, 1, prev));
-    }
-
+    int next_h = height[idx] + S;
+    int next_idx = std::lower_bound(height.begin(), height.end(), next_h) - height.begin();
+    ret = std::max(DP(idx + 1), DP(next_idx) + images[idx].second);
     return ret;
 }
 
@@ -41,15 +32,10 @@ int main() {
         images.push_back({h, c});
     }
     std::sort(images.begin(), images.end());
-    for(int i = 0; i < N; ++i){
-        printf("%d %d\n", images[i].first, images[i].second);
-    }
-    puts("");
+    for(int i = 0; i < images.size(); ++i)
+        height.push_back(images[i].first);
     memset(cache, -1, sizeof(cache));
 
-    int ans = std::max(DP(0, 0, 0), DP(0, 1, 0));
-    printf("%d\n", ans);
-    for(int i = 0; i < N; ++i)
-        printf("%d %d\n", cache[i][0], cache[i][1]);
+    printf("%d\n", DP(0));
     return 0;
 }
