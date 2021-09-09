234b7adefd0de0a1f4719616e658708f419f8dfd
Yuhyun Kim <53094801+Idontknowrithm@users.noreply.github.com>
2021-09-09 23:11:21 +0900

    Update BOJ-2613-숫자구슬.cpp

--- BOJ/BOJ-2613-숫자구슬.cpp
+++ BOJ/BOJ-2613-숫자구슬.cpp
@@ -1,33 +1,67 @@
+// DP가 아니고 쌩 이분탐색이다...
+
 #include<iostream>
-#include<cstring>
+#include<vector>
 
-const int MAX = 305, INF = 300000;
-int N, M, marbles[MAX], sum[MAX], cache[MAX][MAX];
+const int MAX = 305;
+int N, M, marbles[MAX];
 
-int DP(int idx, int packages){
-    if(packages == 1) return sum[idx];
+void solve(){
+    int start = 0, end = 30000, mid;
+    std::vector<int> group, count;
 
-    int &ret = cache[idx][packages];
-    if(ret != -1) return ret;
-    ret = 0;
-    for(int i = 1; idx - i >= packages - 1; ++i){
-        ret = std::max(DP(idx - i, packages - 1), sum[idx] - sum[idx - i]);
+    while(start <= end){
+        mid = (start + end) / 2;
+        group.clear();
+        count.clear();
+        group.push_back(0);
+        count.push_back(0);
+        int size = 0;
+        for(int i = 0; i < N; ++i){
+            if(group[size] + marbles[i] > mid){
+                if(!group[size]){
+                    group[size] += marbles[i];
+                    count[size] += 1;
+                }
+                else{
+                    ++size;
+                    group.push_back(0);
+                    group[size] += marbles[i];
+                    count.push_back(1);
+                }
+            }
+            else{
+                group[size] += marbles[i];
+                count[size] += 1;
+            }
+        }
+        // if(size + 1 == M){
+        //     int max = 0;
+        //     for(int i = 0; i < size + 1; ++i)
+        //         max = std::max(max, group[i]);
+        //     printf("%d\n", max);
+        //     for(int i = 0; i < count.size(); ++i)
+        //         printf("%d ", count[i]);
+        //     return;
+        // }
+        if(size + 1 <= M)
+            end = mid - 1;
+        else
+            start = mid + 1;
     }
-    return ret;
+    int max = 0;
+    for(int i = 0; i < group.size(); ++i)
+        max = std::max(max, group[i]);
+    printf("%d\n", max);
+    for(int i = 0; i < count.size(); ++i)
+        printf("%d ", count[i]);
 }
 
 int main() {
     scanf("%d %d", &N, &M);
     for(int i = 0; i < N; ++i){
         scanf("%d", &marbles[i]);
-        sum[i + 1] = sum[i] + marbles[i];
     }
-        
-    memset(cache, -1, sizeof(cache));
-    int ans = INF;
-    for(int i = 1; N - i >= M - 1; ++i){
-        ans = std::min(ans, std::max(DP(N - i, M - 1), sum[N] - sum[N - i]));
-    }
-    printf("%d", ans);
+    solve();
     return 0;
 }
