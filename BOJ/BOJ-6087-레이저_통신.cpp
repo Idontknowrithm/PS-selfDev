f891c0c17921ea119571569dd317f72ac273a9c1
Yuhyun Kim <53094801+Idontknowrithm@users.noreply.github.com>
2021-09-20 17:04:31 +0900

    Update BOJ-6087-레이저_통신.cpp

--- BOJ/BOJ-6087-레이저_통신.cpp
+++ BOJ/BOJ-6087-레이저_통신.cpp
@@ -1,42 +1,52 @@
+// DP로 생각했는데 풀리지 않아서 해답 알고리즘 봄
+// visited 배열을 DP 캐시처럼 이용하여 해결함
+
 #include<iostream>
 #include<algorithm>
 #include<vector>
 #include<cstring>
+#include<queue>
 #define pp std::pair<int, int>
+
+typedef struct{
+    int r, c, mirror, dir;
+}data;
 
 const int y[4] = {1, 0, -1, 0};
 const int x[4] = {0, 1, 0, -1};
 const int MAX = 105, INF = 100005;
-int H, W, map[MAX][MAX], visited[MAX][MAX], cache[MAX][MAX][5];
+int H, W, map[MAX][MAX], cache[MAX][MAX][5];
 std::string input;
 std::vector<pp> Cs;
 
 bool possible(int r, int c){
     return r >= 0 && r < H && c >= 0 && c < W && map[r][c] != 1;
 }
-int DP(int r, int c, int prev){
-    if(r == Cs[1].first && c == Cs[1].second) return 0;
 
-    int &ret = cache[r][c][prev];
-    if(ret != -1) return ret;
-    ret = INF;
-
+int route(){
+    std::queue<data> bfs;
     for(int i = 0; i < 4; ++i){
-        int R = r + y[i], C = c + x[i];
-        if(possible(R, C) && !visited[R][C]){
-            if(prev == 4 || prev == i){
-                visited[R][C] = 1;
-                ret = std::min(ret, DP(R, C, i));
-                visited[R][C] = 0;
-            }   
-            else{
-                visited[R][C] = 1;
-                ret = std::min(ret, 1 + DP(R, C, i));
-                visited[R][C] = 0;
+        int R = Cs[0].first + y[i], C = Cs[0].second + x[i];
+        if(possible(R, C))
+            bfs.push({R, C, 0, i});
+    }
+    while(!bfs.empty()){
+        data tmp = bfs.front();
+        bfs.pop();
+        for(int i = 0; i < 4; ++i){
+            int R = tmp.r + y[i], C = tmp.c + x[i];
+            int cur_mirror = tmp.mirror;
+            if(i != tmp.dir) ++cur_mirror;
+            if(possible(R, C) && cache[R][C][i] > cur_mirror){
+                cache[R][C][i] = cur_mirror;
+                bfs.push({R, C, cur_mirror, i});
             }
         }
     }
-    return ret;
+    int ans = INF;
+    for(int i = 0; i < 4; ++i)
+        ans = std::min(ans, cache[Cs[1].first][Cs[1].second][i]);
+    return ans;
 }
 
 int main() {
@@ -53,8 +63,11 @@ int main() {
                 Cs.push_back({i, u});
             }
     }
-    memset(cache, -1, sizeof(cache));
-    visited[Cs[0].first][Cs[0].second] = 1;
-    std::cout << DP(Cs[0].first, Cs[0].second, 4);
+    for(int i = 0; i <= H; ++i)
+    for(int u = 0; u <= W; ++u)
+    for(int k = 0; k < 4; ++k)
+        cache[i][u][k] = INF;
+    
+    std::cout << route();
     return 0;
 }
