6c3ac911788a5238f1329bd36ff65497021b084f
Yuhyun Kim <53094801+Idontknowrithm@users.noreply.github.com>
2021-10-06 20:03:22 +0900

    Update BOJ-1039-교환.cpp

--- BOJ/BOJ-1039-교환.cpp
+++ BOJ/BOJ-1039-교환.cpp
@@ -1,27 +1,15 @@
+// 우와... 어렵다
+
 #include<iostream>
 #include<algorithm>
 #include<vector>
+#include<queue>
+#define pp std::pair<int, int>
 
 const int MAX = 1000005, digit_10[7] = {1, 10, 100, 1000, 10000, 100000, 1000000};
 int N, K, digit, ans = -1;
-bool num_cache[MAX];
+bool num_cache[MAX], possible_num[MAX];
 
-void caching(int n){
-    std::vector<int> num;
-    while(n){
-        num.push_back(n % 10);
-        n /= 10;
-    }
-    std::sort(num.begin(), num.end());
-    do{
-        int tmp = 0;
-        for(int i = 0; i < num.size(); ++i){
-            tmp *= 10;
-            tmp += num[i];
-        }
-        num_cache[tmp] = true;
-    }while(std::next_permutation(num.begin(), num.end()));
-}
 int digit_cal(int n){
     int d = 0;
     while(n){
@@ -38,32 +26,73 @@ int exchange(int n, int i, int u){
     return n;
 }
 void DFS(int n, int k){
-    if(digit_cal(n) != digit || k < 0)
-            return;
+    if(digit_cal(n) != digit || k < 0 || num_cache[n])
+        return;
+    //printf("%d %d\n", n, k);
     if(!k){
-        ans = std::max(ans, n);
+        possible_num[n] = true;
         return;
     }
     for(int i = 0; i < digit - 1; ++i){
         for(int u = i + 1; u < digit; ++u){
             int tmp_n = exchange(n, i, u);
+            if(!(k & 1)){
+                num_cache[n] = true;
+                possible_num[n] = true;
+            }
             DFS(tmp_n, k - 1);
-            DFS(tmp_n, k - 3);
-            DFS(tmp_n, k - 5);
-            DFS(tmp_n, k - 7);
-            DFS(tmp_n, k - 9);
+            if(!(k & 1))
+                num_cache[n] = false;
+            // DFS(tmp_n, k - 3);
+            // DFS(tmp_n, k - 5);
+            // DFS(tmp_n, k - 7);
+            // DFS(tmp_n, k - 9);
         }
     }
 }
-void solve(){
+void search(int n, int k){
+    std::queue<pp> bfs;
+    bfs.push({n, k});
+    if(!(k & 1))
+        num_cache[n] = true;
+    while(!bfs.empty()){
+        pp tmp = bfs.front();
+        bfs.pop();
+        if(num_cache[tmp.first]) continue;
+        // if(!(tmp.second & 1)){
+        //     num_cache[tmp.first] = true;
+        // }
+        for(int i = 0; i < digit - 1; ++i)
+            for(int u = i + 1; u < digit; ++u){
+                int t = exchange(tmp.first, i, u);
+                if(!num_cache[t])
+                    bfs.push({t, k - 1});
+                if(!(k & 1))
+                    num_cache[t] = true;
+            }
+    }
+}
+int solve(){
+    int ans = 0;
+    for(int i = MAX - 5; i >= 0; --i)
+        if(num_cache[i]){
+            ans = i;
+            break;
+        }
     
+    return (digit_cal(ans) == digit_cal(N)) ? ans : -1;
 }
 
 int main() {
     scanf("%d %d", &N, &K);
     digit = digit_cal(N);
-    caching(N);
-    DFS(N, K);
-    printf("%d", ans);
+    if(N / 10 == 0 || (N < 100 && N % 10 == 0)){
+        printf("-1");
+        return 0;
+    }
+    //caching(N);
+    // DFS(N, K);
+    search(N, K);
+    printf("%d", solve());
     return 0;
 }
