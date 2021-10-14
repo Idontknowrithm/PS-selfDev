01bc9ffae4a94a8091dfdab4869487b858fd0f01
Yuhyun Kim <53094801+Idontknowrithm@users.noreply.github.com>
2021-10-14 19:59:16 +0900

    Update BOJ-17040-The_Great_Revegetation_(Bronze).cpp

--- BOJ/BOJ-17040-The_Great_Revegetation_(Bronze).cpp
+++ BOJ/BOJ-17040-The_Great_Revegetation_(Bronze).cpp
@@ -1,34 +1,45 @@
+// 쉬운 알고리즘이라 추측되는데 데이터 제한이 작으면
+// 다 해보는 쪽으로 방향을 잡아야 함
+
 #include<iostream>
-#include<algorithm>
 #include<vector>
-#include<cstring>
+#include<algorithm>
 #define pp std::pair<int, int>
 
 const int MAX = 105;
-int N, M, pastures[MAX];
-std::vector<pp> favors;
-
+int N, M, possible[MAX][5];
+std::vector<pp> cows;
+std::vector<int> graph[MAX];
 
 int main() {
     scanf("%d %d", &N, &M);
     for(int i = 0; i < M; ++i){
         int a, b;
         scanf("%d %d", &a, &b);
-        if(a < b)
-            favors.push_back({a, b});
+        graph[a].push_back(b);
+        graph[b].push_back(a);
+        if(a > b)
+            cows.push_back({b, a});
         else
-            favors.push_back({b, a});
+            cows.push_back({a, b});
     }
-    std::sort(favors.begin(), favors.end());
-    for(int i = 0; i <= N; ++i)
-        pastures[i] = 1;
-    for(int u = 0; u < 150; ++u)
-        for(int i = 0; i < favors.size(); ++i){
-            if(favors[i].first < favors[i].second && 
-            pastures[favors[i].second] == pastures[favors[i].first])
-                pastures[favors[i].second] = pastures[favors[i].first] + 1;
-        }
+    std::sort(cows.begin(), cows.end());
+    for(int i = 1; i <= N; ++i){
+        for(int k = 1; k <= 4; ++k)
+            if(!possible[i][k]){
+                possible[i][k] = 1;
+                for(int u = 0; u < graph[i].size(); ++u)
+                    if(!possible[graph[i][u]][k])
+                        possible[graph[i][u]][k] = -1;
+                break;
+            }
+    }
     for(int i = 1; i <= N; ++i)
-        printf("%d", pastures[i]);
+        for(int u = 1; u <= 4; ++u)
+            if(possible[i][u] == 1){
+                printf("%d", u);
+                break;
+            }
+    
     return 0;
 }
