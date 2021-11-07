eeef161f114de88d9891200679b8aef0e740c8f1
Yuhyun Kim <53094801+Idontknowrithm@users.noreply.github.com>
2021-11-07 14:07:18 +0900

    Update BOJ-11780-플로이드_2.cpp

--- BOJ/BOJ-11780-플로이드_2.cpp
+++ BOJ/BOJ-11780-플로이드_2.cpp
@@ -1,54 +1,65 @@
+// 반복으로 답이 안나올 것 같거나 구현이 어렵게 느껴진다면
+// 재귀로의 전환을 생각해봐야함
+
 #include<iostream>
+#include<algorithm>
 #include<cstring>
 #include<vector>
 
+const int INF = 1000000005;
 int N, M, graph[105][105], floyd[105][105], cache[105][105];
+std::vector<int> trace;
+
+void path_finder(int start, int end){
+    if(!cache[start][end]){
+        trace.push_back(start);
+        trace.push_back(end);
+        return;
+    }
+    path_finder(start, cache[start][end]);
+    trace.pop_back();
+    path_finder(cache[start][end], end);
+}
 
 int main() {
     scanf("%d %d", &N, &M);
+    for(int i = 1; i <= N; ++i)
+    for(int u = 1; u <= N; ++u)
+        graph[i][u] = INF;
+
     for(int i = 0; i < M; ++i){
         int a, b, c;
         scanf("%d %d %d", &a, &b, &c);
-        graph[a][b] = c;
+        graph[a][b] = std::min(graph[a][b], c);
     }
-    for(int i = 1; i <= N; ++i)
-    for(int u = 1; u <= N; ++u)
-        floyd[i][u] = (i == u) ? 0 : 1000000005;
-
-    for(int i = 1; i <= N; ++i){
-        for(int u = 1; u <= N; ++u){
-            if(i == u) continue;
-            for(int k = 1; k <= N; ++k){
-                if(i == k || k == u || !graph[i][k] || !graph[k][u]) continue;
-                if(!graph[i][u] || graph[i][k] + graph[k][u] < graph[i][u]){
+    
+    for(int k = 1; k <= N; ++k){
+        for(int i = 1; i <= N; ++i){
+            for(int u = 1; u <= N; ++u){
+                if(i == u) continue;
+                if(graph[i][k] + graph[k][u] < graph[i][u]){
                     graph[i][u] = graph[i][k] + graph[k][u];
                     cache[i][u] = k;
-                    cache[k][u] = u;
                 }
             }
         }
     }
     for(int i = 1; i <= N; ++i){
         for(int u = 1; u <= N; ++u)
-            printf("%d ", graph[i][u]);
+            printf("%d ", (graph[i][u] == INF) ? 0 : graph[i][u]);
         puts("");
     }
     for(int i = 1; i <= N; ++i){
         for(int u = 1; u <= N; ++u){
-            if(!cache[i][u]){
+            if(graph[i][u] == INF){
                 printf("0\n");
             }
             else{
-                std::vector<int> trace;
-                int tmp = cache[i][u];
-                while(tmp != u){
-                    trace.push_back(tmp);
-                    tmp = cache[tmp][u];
-                }
-                trace.push_back(tmp);
+                trace.clear();
+                path_finder(i, u);
                 printf("%d ", trace.size());
                 for(int k = 0; k < trace.size(); ++k)
-                    printf("%d ", trace[i]);
+                    printf("%d ", trace[k]);
                 puts("");
             }
         }
