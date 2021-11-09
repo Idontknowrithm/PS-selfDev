0a9ea1b04f2a28b646f5de5e41250a880e0fe36d
Yuhyun Kim <53094801+Idontknowrithm@users.noreply.github.com>
2021-11-09 17:10:20 +0900

    Update BOJ-1225-이상한_곱셈.cpp

--- BOJ/BOJ-1225-이상한_곱셈.cpp
+++ BOJ/BOJ-1225-이상한_곱셈.cpp
@@ -1,17 +1,18 @@
 #include<iostream>
+#define ll long long
 
 std::string a, b;
-int ans, A[10005], B[10005];
+ll ans, A[10005], B[10005];
 
 int main() {
     std::cin >> a >> b;
-    for(int i = 0; i < a.size(); ++i)
+    for(ll i = 0; i < a.size(); ++i)
         A[i] = a[i] - '0';
-    for(int i = 0; i < b.size(); ++i)
+    for(ll i = 0; i < b.size(); ++i)
         B[i] = b[i] - '0';
-    for(int i = 0; i < a.size(); ++i)
-    for(int i = 0; i < b.size(); ++i)
-        ans += A[i] * B[i];
+    for(ll i = 0; i < a.size(); ++i)
+    for(ll u = 0; u < b.size(); ++u)
+        ans += A[i] * B[u];
     
     std::cout << ans;
     return 0;
