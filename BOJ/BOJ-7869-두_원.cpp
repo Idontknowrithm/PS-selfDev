283d4aaa56128bdc78c32b6dea7cfbb38b499d76
Yuhyun Kim <53094801+Idontknowrithm@users.noreply.github.com>
2021-11-17 19:14:23 +0900

    Update BOJ-7869-두_원.cpp

--- BOJ/BOJ-7869-두_원.cpp
+++ BOJ/BOJ-7869-두_원.cpp
@@ -1,4 +1,4 @@
-// 작은 원의 중심이 큰 원 안에 들어있으면서 약간 겹치는 경우 남았음
+// 공식 유도 중 오류가 난 듯
 
 #include<iostream>
 #include<algorithm>
@@ -20,8 +20,8 @@ int main() {
         ans = M_PI * r1 * r1;
         printf("%.3lf", ans);
     }
-    else if(len_between > r2){
-        double hi = 2 * M_PI, lo = 0;
+    else if(r1 * r1 + len_between * len_between >= r2 * r2){
+        double hi = M_PI / 2, lo = 0;
         for(int i = 0; i < 500; ++i){
             double mid = (hi + lo) / 2;
             double exp = r2 * cos(mid) + r1 * sqrt(1 - (r2 * r2)/(r1 * r1) * sin(mid) * sin(mid));
@@ -31,8 +31,25 @@ int main() {
                 hi = mid;
         }
         double l1 = r2 * cos(hi), l2 = len_between - l1;
-        ans = 0.5 * r2 * r2 * hi - l2 * r2 * sin(hi);
-        ans += 0.5 * r1 * r1 * acos(l1 / r1) - l1 * sqrt(r1 * r1 - l1 * l1);
+        ans = r2 * r2 * hi - l2 * r2 * sin(hi);
+        ans += r1 * r1 * acos(l1 / r1) - l1 * sqrt(r1 * r1 - l1 * l1);
+        printf("%.3lf", ans);
+    }
+    else{
+        double hi = M_PI / 2, lo = 0;
+        for(int i = 0; i < 100; ++i){
+            double mid = (hi + lo) / 2;
+            double exp = r1 * r1 * sin(mid) * sin(mid) + len_between * len_between + 
+                         2 * len_between * r1 * cos(mid) + r1 * r1 * cos(mid) * cos(mid);
+            if(exp > r2 * r2)
+                lo = mid;
+            else 
+                hi = mid;
+        }
+        double theta2 = asin(r1 * sin(hi) / r2);
+        printf("%.3lf\n", theta2);
+        ans = 0.5 * r1 * r1 * (2 * M_PI - 2 * hi) + r1 * r1 * sin(hi) * cos(hi);
+        ans += r2 * r2 * theta2 - r2 * r2 * sin(theta2) * cos(theta2);
         printf("%.3lf", ans);
     }
     return 0;
