128525f67514b885ed8bc1225105b45ce1a32c53
Yuhyun Kim <53094801+Idontknowrithm@users.noreply.github.com>
2021-11-22 19:31:34 +0900

    Update BOJ-1086-박성원.cpp

--- BOJ/BOJ-1086-박성원.cpp
+++ BOJ/BOJ-1086-박성원.cpp
@@ -1,32 +1,30 @@
+자잘한 구현과 수학때문에 정말 풀기싫은 유형이다. 문자열에서
+나머지를 구하는 함수에서 문제가 있어서 세그폴트가 뜬 것 같다
+
 #include<iostream>
 #include<algorithm>
 #include<cstring>
+#define ll long long
 
-int N, K, cache[105][(1 << 15) + 5], mod[20], ten_mod[55];
-std::string nums[20];
+ll N, K, cache[105][(1 << 16)], mod[50], ten_mod[100];
+std::string nums[50];
 
-int GCD(int a, int b){
-    if(a > b) std::swap(a, b);
-    if(!b) return a;
-    return GCD(b, a % b);
+ll GCD(ll a, ll b){
+    ll A, B;
+    A = std::max(a, b);
+    B = std::min(a, b);
+
+    while(B){
+        ll tmp = A % B;
+        A = B;
+        B = tmp;
+    }
+    return A;
 }
 
-int modulo(std::string a, int k){
-    int len = 0, tmp = k;
-    while(tmp){
-        tmp /= 10;
-        ++len;
-    }
-    tmp = 0;
-    if(len == 1)
-        tmp = a[0] - '0';
-    else if(len == 2)
-        tmp = (a[0] - '0') * 10 + a[1] - '0';
-    else
-        tmp = (a[0] - '0') * 100 + (a[1] - '0') * 10 + a[2] - '0';
-
-    tmp %= k;
-    for(int i = len; i < a.size(); ++i){
+ll modulo(std::string a, ll k){
+    ll tmp = 0;
+    for(ll i = 0; i < a.size(); ++i){
         tmp *= 10;
         tmp += a[i] - '0';
         tmp %= k;
@@ -34,16 +32,14 @@ int modulo(std::string a, int k){
     return tmp;
 }
 
-int DP(){
+void DP(){
     cache[0][0] = 1;
-    for(int u = 0; u < (1 << N) - 1; ++u){
-        std::cout << "ok\n";
-        for(int k = 0; k < N; ++k){
-            std::cout << k << "\n";
+    for(ll u = 0; u < (1 << N) - 1; ++u){
+        for(ll k = 0; k < N; ++k){
             if(!(u & (1 << k))){
-                int next = u | (1 << k);
-                for(int i = 0; i < K; ++i){
-                    int new_mod = ((i * ten_mod[nums[k].size()]) % K + modulo(nums[k], K)) % K;
+                ll next = u | (1 << k);
+                for(ll i = 0; i < K; ++i){
+                    ll new_mod = ((i * ten_mod[nums[k].size()]) % K + mod[k]) % K;
                     cache[new_mod][next] += cache[i][u];
                 }
             }
@@ -52,26 +48,26 @@ int DP(){
 }
 
 int main() {
-    int gcd, all = 1, ans = 0;
+    ll gcd, all = 1, ans = 0;
 
     std::cin >> N;
-    for(int i = 0; i < N; ++i)
+    for(ll i = 0; i < N; ++i)
         std::cin >> nums[i];
     std::cin >> K;
-    for(int i = 0; i < N; ++i)
+    for(ll i = 0; i < N; ++i)
         mod[i] = modulo(nums[i], K);
     
-    int ten = 1;
+    ll ten = 1;
     ten_mod[0] = 1 % K;
-    for(int i = 1; i <= 50; ++i){
+    for(ll i = 1; i <= 60; ++i){
         ten *= 10;
         ten_mod[i] = ten % K;
         ten %= K;
     }
     DP();
-    for(int i = 1; i <= N; ++i)
+    for(ll i = 1; i <= N; ++i)
         all *= i;
-    ans = cache[(1 << N) - 1][0];
+    ans = cache[0][(1 << N) - 1];
     gcd = GCD(all, ans);
     all /= gcd;
     ans /= gcd;
