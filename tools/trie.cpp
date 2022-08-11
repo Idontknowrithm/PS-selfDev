8213f9ff4e4112b2ae61fee6e9647ad7b1aac701
Yuhyun Kim <53094801+Idontknowrithm@users.noreply.github.com>
2022-08-11 13:27:34 +0900

    Update trie.cpp

--- tools/trie.cpp
+++ tools/trie.cpp
@@ -2,14 +2,17 @@
 #include<cstring>
 
 const int ALPHABETS = 26;
+
 int ctoi(char ch){
-    return ch - 'A';
+    return ch - 'a';
 }
 
 class trie_node{
-    trie_node* children[ALPHABETS];
+    trie_node *children[ALPHABETS];
     // Is it end node?
     bool terminal;
+    
+    public:
     trie_node() : terminal(false){
         memset(children, 0, sizeof(children));
     }
@@ -19,23 +22,28 @@ class trie_node{
                 delete children[i];
     }
     // add key(string) to trie whose root is this node
-    void insert(const char* key){
+    void insert(const char *key){
         // if string is end, then change the value of terminal to true, and terminate
-        if(*key == 0)
+        if(*key == '\0')
             terminal = true;
         else{
             int next = ctoi(*key);
+            
             // if there is not a corresponding child node, create it
-            if(children[next] == NULL)
+            if(children[next] == nullptr)
                 children[next] = new trie_node();
+            
             // recurse by corresponding child node
             children[next]->insert(key + 1);
         }
     }
-    trie_node* find(const char* key){
-        if(*key == 0) return this;
+    trie_node* find(const char *key){
+        if(*key == '\0')
+            return this;
+        
         int next = ctoi(*key);
-        if(children[next] == NULL) return NULL;
+        
+        if(children[next] == nullptr) return nullptr;
         return children[next]->find(key + 1);
     }
 };
